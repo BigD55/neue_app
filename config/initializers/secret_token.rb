5430df50d29804cfeacbbaa50aa67c6bde3d1c39
Dieter Maas <dieter.maas@gmx.net>
2014-01-28 20:14:14 +0100

    Ablage initialisieren

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NeueApp::Application.config.secret_key_base = '93fd083202bb3e1212ad0e85c706165e86729f35453a27335af1df991b5807e0b06f5005b2112d48c8c6bc6120c7e8e9a65b151a0c8880b534801428996f930b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NeueApp::Application.config.session_store :cookie_store, key: '_neue_app_session'
